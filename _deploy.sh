df3e197b3faaca01d8ccf3a1c54292c23e17a825
Alex Pawlowski <apawlows@vols.utk.edu>
2016-12-09 15:33:55 -0500

    Change deploy shell to have folder right

--- _deploy.sh
+++ _deploy.sh
@@ -5,12 +5,12 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+git config --global user.email "apawlows@vols.utk.edu"
+git config --global user.name "Alex Pawlowski"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
-cp -r ../_book/* ./
+cp -r ../_docs/* ./
 git add --all *
 git commit -m"Update the book" || true
 git push origin gh-pages
