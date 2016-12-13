830e7350ac41c0753656dd9ab134ac4b5cd7e85e
Alex Pawlowski <apawlows@vols.utk.edu>
2016-12-13 15:22:06 -0500

    Pushed app to shinyapps.io and simplified repo structure [skip ci]
    TDIL that `[skip ci]` skips Travis CI Builds. Sweet

---
+++ shiny-app/ui.R
@@ -0,0 +1,37 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(leaflet)
+library(maps)
+library(dplyr)
+
+#checkpoint::checkpoint("2016-12-10")
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("My Clean Power"),
+
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(
+              selectizeInput("fuelTypeInput", #inputID
+                             label = "Power Type", #label
+                             choices = c("BIT"), #weird issue with jquery with shiny where it seems like this can't be null.
+                             selected = "BIT", #this is also weirdly needed.
+                             multiple = TRUE,
+                             options = list(placeholder = 'select a power type'))
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(
+      leafletOutput("map")
+    )
+  )
+))
